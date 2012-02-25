b3f70568b1ba2e77e7341e88d4e33095923879aa
Paul J. Davis <paul.joseph.davis@gmail.com>
2012-02-25 15:11:35 -0600

    Allow Python to build and run Erlang scripts
    Sends the necessary scipts to Erlang to be compiled and run. The compile
    option returns an identifier for the code that can be later passed to
    the run function. Alternatively, the script itself can be passed
    directly to the run function.
    
    Scripts to be run should mimic a escript with no header. Ie, they must
    define a main/1 function that will be called with the provided argument.
    The result of the main/1 function is returned to Python. An example
    script might look something like:
    
        main(_) ->
            {ok, erlang:now()}.
    
    Which would return the current timestamp from Erlang.

--- py/erlang/vm.py
+++ py/erlang/vm.py
@@ -4,9 +4,13 @@ import subprocess as sp
 import sys
 
 
+import erlang
+a = erlang.Atom
+
+
 class VM(object):
     def __init__(self):
-        pass
+        self.booted = False
 
     def boot(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,4 +22,41 @@ class VM(object):
         self.pipe = sp.Popen(" ".join(cmd), shell=True)
         self.conn, _ = sock.accept()
         sock.close()
+        self.booted = True
 
+    def call(self, mod, fun, args, timeout=None):
+        if timeout is None:
+            timeout = a.infinity
+        body = tuple(a.call, timeout, a(mod), a(fun), args)
+        return self.request(body)
+
+    def compile(self, script):
+        modname = "nutty_" + hashlibg.sha1(script).hexdigest().upper()
+        script = "-module(%s).\n-export([main/0]).\n\n" + script
+        body = tuple(a.compile, Atom(modname), script)
+        resp = self.request(body)
+        if resp != a.ok:
+            raise ValueError("Error compiling script: %s" % resp)
+        return modname
+
+    def run(self, script, arg=None, timeout=None):
+        if not (script.startswith("nutty_") and len(script) == 46):
+            script = self.compile(script)
+        if timeout is None:
+            timeout = a.infinity
+        body = tuple(a.run, timeout, Atom(modname), arg)
+        return self.request(body)
+
+    def request(self, body):
+        body = erlang.serialize(body)
+        data = struct.pack("!I", len(body)) + body
+        self.conn.sendall(data)
+        buf = ""
+        while len(buf) < 4:
+            self.conn.recv(4 - len(buf))
+        size = struct.unpack("!I", buf)[0]
+        ret = []
+        while size > 0:
+            ret.append(self.conn.recv(size))
+            size -= len(ret[-1])
+        return "".join(ret)
