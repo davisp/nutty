7cf956387f93413c8ed3087ee5c001e4396179a3
Paul J. Davis <paul.joseph.davis@gmail.com>
2012-02-25 23:13:32 -0600

    New repeated test

--- test/02-test-errors.py
+++ test/02-test-errors.py
@@ -5,11 +5,18 @@ import h
 import erlang
 a = erlang.Atom
 
+
 @h.vm()
 def test_no_fun(vm):
     t.eq(vm.call(a.foo, a.bar)[0], a.undef)
 
+
 @h.vm()
 def test_syntax_error(vm):
-    script = "main(_) foo."
-    t.raises(ValueError, vm.compile, script)
+    t.raises(ValueError, vm.compile, "main(_) foo.")
+
+
+@h.vm()
+def test_repeated_errors(vm):
+    for i in range(100):
+        t.raises(ValueError, vm.compile, "main(_) foo.")
